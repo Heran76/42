921f06425e1473947de871e6ed8defdb59c2a907
Heran76 <heran76@gmail.com>
2024-09-30 19:49:00 +0200

    modifiedIsprint

--- libft_proyecto/libft/ft_isprint.c
+++ libft_proyecto/libft/ft_isprint.c
@@ -7,32 +7,26 @@ inprimibles, como espacios o caracteres de control
 
 #include "libft.h"
 #include <stdio.h>
+
 int ft_isprint(int c)
 {
-    if(c >=32 && c <= 126)
+    if(c >=32 && c <=126)
      return(1);
     return(0); 
 }
 
-int main()
-{
-    int c = 65;
-    int b = 140;
-    int d = ' ';
-
-    if(ft_isprint(c))
-     printf("El caracter %c es imprimible \n",c);
-    else
-     printf("El caracter %c no es imprimible \n",c);
-
-     if(ft_isprint(b))
-     printf("El caracter %c es imprimible \n",b);
-     else
-     printf("El caracter %c no es imprible \n",b);
-
-     if(ft_isprint(d))
-      printf("El caracter %c es imprimible \n",d);
-     else
-      printf("El caracter %c no es imprimible \n",d);
-  return 0;      
+int main(){
+    
+    char test_char[]={65,35,48,40,32};
+    int num_test = sizeof(test_char) / sizeof(test_char[0]);
+    
+    for(int i = 0; i <num_test; i++)
+    {
+        if(ft_isprint(test_char[i]))
+         printf("ft_isprint(%d) = %c es imprible.\n ",test_char[i],test_char[i]);
+        else
+         printf("ft_isprint(%d) = %c no es imprimible.\n ",test_char[i], test_char[i]);
+         
+    }
+    return 0;
 }
